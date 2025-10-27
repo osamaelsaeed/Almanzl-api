ea69c49f60a8ff2d9df69d6967b083d444cef7b2
Terror-1 <elfarhosam487@gmail.com>
2025-10-27 20:39:10 +0300

    setup .env and connection to db

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import express from 'express';
+import "dotenv/config";
+import express from "express";
+import initDB from "./src/config/db.js";
+import requestLogger from "./src/middlewares/requestLogger.js";
+
+import { PORT, NODE_ENV } from "./src/config/config.js";
+
+initDB();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger);
 
-
-
-app.listen(3000, () => {
-    console.log('App Listining at port 3000');
+app.listen(PORT, () => {
+  console.log(`app Listining at port ${PORT} (mode: ${NODE_ENV}) `);
 });
