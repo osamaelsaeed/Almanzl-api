93edb0eda703fc082b00685b401cf0bac93477be
HassanAli381 <alhassanali599@gmail.com>
2025-11-04 20:03:53 +0200

    Adding headers to configure CORS

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ app.use(cors({
     },
     credentials: true
 }));
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    next();
+});
 
 if (NODE_ENV === 'development') {
     app.use(morgan('dev'));
