27b6ee3dc92b6fc6be4a2eb99aa7c5a4e0dc0df6
yousefmobelal <ym8370302@gmail.com>
2025-11-04 09:05:45 +0200

    add functionality to add product to favorites

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,15 @@
 import User from '../models/user.model';
 import { asyncHandler } from '../utils/asyncHandler';
+import { SUCCESS } from '../utils/reposnseStatus';
 
-export const addProductToFavorite = asyncHandler(async (req, res, next) => {
+export const addProductToFavorites = asyncHandler(async (req, res, next) => {
     const { productId } = req.params;
-    const updatedUser = req.user.favorites.push(productId);
-    const user = await User.findOneAndUpdate(req.id, updatedUser);
+
+    req.user.favorites.push(productId);
+    const user = await User.findOneAndUpdate(req.id, req.user);
+
+    res.status(201).json({
+        status: SUCCESS,
+        data: user,
+    });
 });
