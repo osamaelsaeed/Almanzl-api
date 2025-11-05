51c4a8ddef94810c3442f25ae58b218e09f2776a
Terror-1 <elfarhosam487@gmail.com>
2025-11-06 01:56:12 +0200

    update nodemailer to send in production

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,19 +1,34 @@
 import nodemailer from 'nodemailer';
+import BrevoTransport from 'nodemailer-brevo-transport';
+import { NODE_ENV } from '../config/config.js';
 
-export const sendEmail = async ({ to, subject, text, message }) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: false,
-        auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
-    });
+export const sendEmail = async ({ to, subject, text, html }) => {
+    let transporter;
+
+    if (NODE_ENV === 'production') {
+        transporter = nodemailer.createTransport(
+            new BrevoTransport({
+                apiKey: process.env.BREVO_API_KEY,
+            })
+        );
+    } else {
+        transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+    }
 
     const mailOptions = {
         from: process.env.EMAIL_FROM || 'Support <no-reply@almanzel.com>',
         to,
         subject,
         text,
-        message,
+        html,
     };
 
     await transporter.sendMail(mailOptions);
