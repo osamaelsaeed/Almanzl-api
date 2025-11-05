e6888f8e37dd47a359d5ea089e0ad6c9a39aac87
yousefmobelal <ym8370302@gmail.com>
2025-11-05 11:03:14 +0200

    add favoritesCount to product

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -57,6 +57,10 @@ const productSchema = new mongoose.Schema(
             type: Number,
             default: 0,
         },
+        favoritesCount: {
+            type: Number,
+            default: 0,
+        },
     },
     {
         timestamps: true,
