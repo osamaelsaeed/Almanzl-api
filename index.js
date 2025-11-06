641382bfb4681cf232dfc5b82acd6eeffde853f8
yousefmobelal <ym8370302@gmail.com>
2025-11-07 00:47:53 +0200

    fix: enhance Swagger UI setup with custom CSS for improved styling

--- index.js
+++ index.js
@@ -50,8 +50,17 @@ if (NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
 
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, {
+        customCss:
+            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+        customCssUrl: CSS_URL,
+    })
+);
 app.use('/api/products', productRouter);
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
